c0fcf9fcf658d4890759adb32e6851f3827664f3
hipeople <dongfong9@gmail.com>
2020-04-16 18:45:47 +0700

    3

--- pages/tag/requiretag.js
+++ pages/tag/requiretag.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const requrieTag = () => {
     return(
         <Layout>
-            <h2 className="text-center pt-4 pb-4">Require Tag</h2>
+            <h2 className="text-center pt-4 pb-4">Request for Tag</h2>
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <RequireTagComponent />
