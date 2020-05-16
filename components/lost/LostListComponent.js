e45f3f606c43e19629857830de328fe1b42232f9
hipeople <dongfong9@gmail.com>
2020-05-16 17:51:41 +0700

    add tailwindcss

--- components/lost/LostListComponent.js
+++ components/lost/LostListComponent.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
-import Router from 'next/router';
 import { listLostPets } from '../../actions/pet';
 import moment from 'moment';
 import { API } from '../../config';
@@ -24,52 +23,11 @@ const LostListComponent = () => {
     };
 
     const showAllLostPets = () => {
-        const style = {
-            width: '80%',
-            padding: '16px',
-            textAlign: 'center',
-            border: '1px solid #eee',
-            boxShadow: '0 2px 3px #ccc',
-            margin: '10px',
-            boxSizing: 'border-box',
-        }
-
-        const post = {
-            display: 'flex',
-            flexFlow: 'row wrap',
-            justifyContent: 'center',
-            width: '80%',
-            margin: 'auto'
-        }
-    
         return pets.map((pet, i) => {
             return (
-                <section style={post}>
-                    <div style={style}>
-                        <div key={i}>
-                            <div className="row">
-                                <div className="col-md-4">
-                                    <section>
-                                        <img className="img img-fluid"
-                                            style={{ maxHeight: '150px', width: 'auto' }}
-                                            src={`${API}/pets/photo/${pet.id}`}
-                                            alt={pet.name}/>
-                                    </section>
-                                </div>
-                                <div className="col-md-8">
-                                    <div style={{padding: '20px'}}>
-                                        <h4 style={{color:'#FC7139'}}>ID: {pet.id}</h4>
-                                        <p>Category: <strong>{pet.categories[0].name}</strong></p>
-                                        <p>Name: <strong>{pet.name}</strong></p>
-                                        <p>Breed: <strong>{pet.breed}</strong></p>
-                                        <p>Gender: <strong>{pet.gender}</strong></p>
-                                        Post by <strong>{pet.postedBy.name} </strong> | Lost on <strong>{moment(pet.updatedAt).fromNow()}</strong>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+               <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                   <img src={`${API}/pets/photo/${pet.id}`} alt={pet.name} className="w-full" />
+               </div>
             );
         });
     };
