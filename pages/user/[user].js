89bbad2ec54b2a45e20dbe2841aafe2ec5fa69e8
hipeople <dongfong9@gmail.com>
2020-05-24 22:51:17 +0700

    update frontend

--- pages/user/[user].js
+++ pages/user/[user].js
@@ -4,13 +4,38 @@ import { withRouter } from 'next/router';
 import { getCookie } from '../../actions/auth';
 import Private from '../../components/auth/Private';
 import { listPets, removePet, lost, find } from '../../actions/pet';
+import moment from 'moment';
 import { API } from '../../config';
 import Link from 'next/link';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
 
-const ListPets = ({ pets }) => {
+const ListPets = ({ pets, query }) => {
 
+    const [pet, setPets] = useState([]);
     const [message, setMessage] = useState('');
     const token = getCookie('token');
+
+    // const loadPets = () => {
+    //     listPets(query.user).then(data => {
+    //         if (data.error) {
+    //             console.log(data.error);
+    //         } else {
+    //             setPets(data);
+    //         }
+    //     });
+    // };
+
+    // useEffect(() => {
+    //     loadPets();
+    // }, []);
 
     const deletePet = id => {
         removePet(id, token).then(data => {
@@ -54,6 +79,7 @@ const ListPets = ({ pets }) => {
         let answer = window.confirm('Are you sure your pet lost? if sure when someone found your pet it will inform at email');
         if (answer) {
             lostPet(id);
+           
         }
     };
 
@@ -66,36 +92,40 @@ const ListPets = ({ pets }) => {
 
     const showAllPets = () => {
         return pets.map((pet, i) => {
-            return (                
-                    <div className="col-lg-4" key={i} style={{marginTop: '80px'}}>
-                        <div className="card fixed-width" style={{width: '18rem'}}>
-                            <img
-                                className="img img-fluid"
-                                style={{ height: '300px', width: '100%'}}
-                                src={`${API}/pets/photo/${pet.id}`}
-                                alt={pet.name}/>
-                            <div className="card-body">
+            return (
+                <div className="container-fluid" style={fluidStyle}>
+                    <div className="mb-3 card">
+                        <div className="no-gutters row">
+                            <div className="col-md-4">
+                                <section>
+                                    <img
+                                        className="img img-fluid"
+                                        style={{ height: '220px', width: '200px', marginTop: '15px', marginBottom: '15px', marginLeft: '15px', justifyContent: 'center' }}
+                                        src={`${API}/pets/photo/${pet.id}`}
+                                        alt={pet.name}/>
+                                </section>
+                            </div>
+                            <div className="col-md-4">
                                 <div className="pb-3" style={nameStyle}>ID: <strong>{pet.id}</strong></div>
-                                <div className="pb-3" style={nameStyle}>Name: <strong>{pet.name}</strong></div>  
-                                <Link href={`/pet/${pet.id}`}>
-                                    <button type="submit" className="btn btn-primary pt-2" style={buttonUpdateStyle}>
-                                        Update profile
-                                    </button>
+                                <div className="pb-3" style={nameStyle2}>Name: <strong>{pet.name}</strong></div>   
+                                <Link href={`/pet/${pet.id}`}> 
+                                <button type="submit" className="btn btn-primary pt-2" style={buttonUpdateStyle}> 
+                                Update profile 
+                                </button> 
                                 </Link>
-                                <br />
-                                <button className="btn btn-warning btn-md" style={buttonLostStyle} onClick={() => lostConfirm(pet.id)} > 
+                                    <button className="btn btn-warning btn-md" style={buttonLostStyle} onClick={() => lostConfirm(pet.id)} > 
                                         Lost
-                                </button>
-                                <button className="btn btn-success btn-md" style={buttonFindStyle} onClick={() => findConfirm(pet.id)}>
+                                    </button>
+                                    <button className="btn btn-success btn-md" style={buttonFindStyle} onClick={() => findConfirm(pet.id)}>
                                         Find
-                                </button>
-                                <br></br>
-                                <button className="btn btn-danger btn-md" style={buttonDeleteStyle} onClick={() => deleteConfirm(pet.id)}>
+                                    </button>
+                                    <button className="btn btn-danger btn-md" style={buttonDeleteStyle} onClick={() => deleteConfirm(pet.id)}>
                                         Delete
-                                </button>
+                                    </button>
                             </div>
                         </div>
                     </div>
+                </div>
             );
         });
     };
@@ -105,13 +135,36 @@ const ListPets = ({ pets }) => {
         <React.Fragment>
         <Layout>
             <Private> 
-            </Private>
-            <div className="container">
+            <div className="app-inner-bar" style={barStyle}>                    
+                <div className="inner-bar-center">
+                    <ul className="nav">
+                        <li className="nav-item">
+                            <a role="tab" data-toggle="tab" className="nav-link active" href="#tab-content-0" aria-selected="true">
+                                <span>Dog</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a role="tab" data-toggle="tab" className="nav-link" href="#tab-content-1" aria-selected="false">
+                                <span>Cat</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a role="tab" data-toggle="tab" className="nav-link" href="#tab-content-2" aria-selected="false">
+                                <span>Etc.</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>                 
+            </div>
+            <div className="container-fluid" style={containerStyle}>
                 <div className="row">
+                    <div className="col-md-12">
                         {message && <div className="alert alert-warning">{message}</div>}
                         {showAllPets()}
+                    </div>
                 </div>
             </div>
+            </Private>
         </Layout>
         </React.Fragment>
     );
@@ -127,35 +180,79 @@ ListPets.getInitialProps = ({ query }) => {
         }
     });
 };
+
+const barStyle = {
+    background: '#fff',
+    border: '#dee2e6 solid 1px',
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: '.35rem',
+    padding: '.5rem 0',
+    marginTop: '80px',
+    marginBottom: '30px',
+    marginLeft: '30px',
+    marginRight: '30px',
+    justifyContent: 'center'
+};
+
+const fluidStyle = {
+    width: '100%',
+    paddingRight: '15px',
+    paddingLeft: '15px',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+};
+
 const nameStyle = {
-    fontSize: '18px',
+    fontSize: '22px',
+    marginTop: '30px',
+    marginLeft: '15px',
+    src: 'url(../../../fonts/BANGNA-NEW.TTF)',
+};
+
+const nameStyle2 = {
+    fontSize: '22px',
+    marginLeft: '15px',
+    src: 'url(../../../fonts/BANGNA-NEW.TTF)',
 };
 
 const buttonUpdateStyle = {
     backgroundColor: '#0384BD',
-    marginBottom: '5px',
     border: 'none',
+    marginLeft: '10px',
+    marginBottom: '15px'
 };
 
 const buttonLostStyle = {
     backgroundColor: '#FEC748',
-    marginBottom: '5px',
     marginRight: '5px',
+    marginLeft: '5px',
     border: 'none',
+    marginBottom: '15px'
 };
 
 const buttonFindStyle = {
     backgroundColor: '#AEC33A',
     color: '#262626',
-    marginBottom: '5px',
     marginRight: '5px',
     border: 'none',
+    marginBottom: '15px'
 };
 
 const buttonDeleteStyle = {
     backgroundColor: '#D92139',
-    marginBottom: '5px',
     border: 'none',
+    marginBottom: '15px'
+};
+
+const containerStyle = {
+    width: '100%',
+    minHeight: '100vh',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: '0',
+    left: '0'
 };
 
 export default withRouter(ListPets);
