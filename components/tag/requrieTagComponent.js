4dbb924eafac430fbb3b4a2fbf8c35af824cbcad
hipeople <dongfong9@gmail.com>
2020-05-28 13:45:52 +0700

    update frontend

--- components/tag/requrieTagComponent.js
+++ components/tag/requrieTagComponent.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { requiretag } from '../../actions/tag';
+import {useState, useEffect} from 'react';
+import {requiretag} from '../../actions/tag';
 import Router from 'next/router';
 
 const RequireTagComponent = () => {
@@ -20,17 +20,43 @@ const RequireTagComponent = () => {
     showForm: true,
   });
 
-  const { name, email, houseNumber, village, road, alley, district, zone, province, postalCode, error, loading, message, showForm } = values;
+  const {
+    name,
+    email,
+    houseNumber,
+    village,
+    road,
+    alley,
+    district,
+    zone,
+    province,
+    postalCode,
+    error,
+    loading,
+    message,
+    showForm,
+  } = values;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // console.table({ name, email, password, error, loading, message, showForm });
-    setValues({ ...values, loading: true, error: false });
-    const tag = { name, email, houseNumber, village, road, alley, district, zone, province, postalCode };
+    setValues({...values, loading: true, error: false});
+    const tag = {
+      name,
+      email,
+      houseNumber,
+      village,
+      road,
+      alley,
+      district,
+      zone,
+      province,
+      postalCode,
+    };
 
     requiretag(tag).then((data) => {
       if (data.error) {
-        setValues({ ...values, error: data.error, loading: false });
+        setValues({...values, error: data.error, loading: false});
       } else {
         setValues({
           ...values,
@@ -54,14 +80,19 @@ const RequireTagComponent = () => {
   };
 
   const handleChange = (name) => (e) => {
-    setValues({ ...values, error: false, [name]: e.target.value });
+    setValues({...values, error: false, [name]: e.target.value});
   };
 
-  const showLoading = () => (loading ? <div className="alert alert-info">Loading...</div> : '');
-  const showError = () => (error ? <div className="alert alert-danger">{error}</div> : '');
+  const showLoading = () =>
+    loading ? <div className="alert alert-info">Loading...</div> : '';
+  const showError = () =>
+    error ? <div className="alert alert-danger">{error}</div> : '';
   const showMessage = () =>
     message ? (
-      <div className="alert alert-info" style={{ marginLeft: '20px', marginTop: '80px' }}>
+      <div
+        className="alert alert-info"
+        style={{marginLeft: '20px', marginTop: '80px'}}
+      >
         {message}
       </div>
     ) : (
@@ -87,11 +118,25 @@ const RequireTagComponent = () => {
           </p>
 
           <div className="form-group">
-            <input value={name} onChange={handleChange('name')} type="text" className="form-control" placeholder="ชื่อ / name" style={inputRequest} />
+            <input
+              value={name}
+              onChange={handleChange('name')}
+              type="text"
+              className="form-control"
+              placeholder="ชื่อ / name"
+              style={inputRequest}
+            />
           </div>
 
           <div className="form-group">
-            <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="อีเมล์ / email" style={inputRequest} />
+            <input
+              value={email}
+              onChange={handleChange('email')}
+              type="email"
+              className="form-control"
+              placeholder="อีเมล์ / email"
+              style={inputRequest}
+            />
           </div>
 
           <div>
@@ -101,31 +146,152 @@ const RequireTagComponent = () => {
             <div className="row">
               <div className="col-md-6">
                 <div className="form-group">
-                  <input value={houseNumber} onChange={handleChange('houseNumber')} type="text" className="form-control" placeholder="บ้านเลขที่ / house number" style={inputRequest} />
+                  <input
+                    value={houseNumber}
+                    onChange={handleChange('houseNumber')}
+                    type="text"
+                    className="form-control"
+                    placeholder="บ้านเลขที่ / house number"
+                    style={inputRequest}
+                  />
                 </div>
               </div>
 
               <div className="col-md-6">
                 <div className="form-group">
-                  <input value={village} onChange={handleChange('village')} type="text" className="form-control" placeholder="ซอย / village" style={inputRequest} />
+                  <input
+                    value={village}
+                    onChange={handleChange('village')}
+                    type="text"
+                    className="form-control"
+                    placeholder="ซอย / village"
+                    style={inputRequest}
+                  />
                 </div>
               </div>
             </div>
 
             <div className="form-group">
-              <input value={road} onChange={handleChange('road')} type="text" className="form-control" placeholder="ถนน / road" style={inputRequest} />
+              <input
+                value={road}
+                onChange={handleChange('road')}
+                type="text"
+                className="form-control"
+                placeholder="ถนน / road"
+                style={inputRequest}
+              />
             </div>
             <div className="form-group">
-              <input value={district} onChange={handleChange('district')} type="text" className="form-control" placeholder="แขวง / district" style={inputRequest} />
+              <input
+                value={district}
+                onChange={handleChange('district')}
+                type="text"
+                className="form-control"
+                placeholder="แขวง / district"
+                style={inputRequest}
+              />
             </div>
             <div className="form-group">
-              <input value={zone} onChange={handleChange('zone')} type="text" className="form-control" placeholder="เขต / zone" style={inputRequest} />
+              <input
+                value={zone}
+                onChange={handleChange('zone')}
+                type="text"
+                className="form-control"
+                placeholder="เขต / zone"
+                style={inputRequest}
+              />
             </div>
             <div className="form-group">
-              <input value={province} onChange={handleChange('province')} type="text" className="form-control" placeholder="จังหวัด / province" style={inputRequest} />
+              <input
+                value={province}
+                onChange={handleChange('province')}
+                type="text"
+                className="form-control"
+                placeholder="จังหวัด / province"
+                style={inputRequest}
+              />
             </div>
             <div className="form-group">
-              <input value={postalCode} onChange={handleChange('postalCode')} type="text" className="form-control" placeholder="รหัสไปรษณีย์ / postalcode" style={inputRequest} />
+              <input
+                value={postalCode}
+                onChange={handleChange('postalCode')}
+                type="text"
+                className="form-control"
+                placeholder="รหัสไปรษณีย์ / postalcode"
+                style={inputRequest}
+              />
+            </div>
+
+            <div className="col-md-4">
+              <p>
+                <strong>รูปแบบของแท็ก / Style of tag</strong>
+              </p>
+              <label style={{marginLeft: '50px'}}>
+                <input type="radio" value="style1" name="styleTag" />
+                Style 1
+              </label>
+              <br />
+              <label style={{marginLeft: '50px'}}>
+                <input type="radio" value="style2" name="styleTag" />
+                Style 2
+              </label>
+              <br />
+              <label style={{marginLeft: '50px'}}>
+                <input type="radio" value="style3" name="styleTag" />
+                Style 3
+              </label>
+            </div>
+
+            <div
+              className="row"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                margin: '0 auto',
+              }}
+            >
+              <div className="col-md-3">
+                <p style={{margin: '20px 80px'}}>"Style1"</p>
+                <img
+                  src="https://sv1.picz.in.th/images/2020/05/28/qc7oaR.jpg"
+                  alt="Style1"
+                  border="0"
+                  style={{
+                    maxWidth: '160px',
+                    maxHeight: '160px',
+                    marginTop: '10px',
+                    marginLeft: '35px',
+                  }}
+                />
+              </div>
+              <div className="col-md-3">
+                <p style={{margin: '20px 80px'}}>"Style2"</p>
+                <img
+                  src="https://sv1.picz.in.th/images/2020/05/28/qc7Pxu.jpg"
+                  alt="Style2"
+                  border="0"
+                  style={{
+                    maxWidth: '150px',
+                    maxHeight: '150px',
+                    marginTop: '10px',
+                    marginLeft: '35px',
+                  }}
+                />
+              </div>
+              <div className="col-md-3">
+                <p style={{margin: '20px 80px'}}>"Style3"</p>
+                <img
+                  src="https://sv1.picz.in.th/images/2020/05/28/qc7CWI.jpg"
+                  alt="Style3"
+                  border="0"
+                  style={{
+                    maxWidth: '160px',
+                    maxHeight: '160px',
+                    marginTop: '10px',
+                    marginLeft: '35px',
+                  }}
+                />
+              </div>
             </div>
           </div>
 
@@ -148,7 +314,6 @@ const RequireTagComponent = () => {
     </React.Fragment>
   );
 };
-
 const containerRequestTag = {
   width: '105%',
   minHeight: '100vh',
